396adc1136199902cc8cfc8522c2da150d7469e8
Bazza <bazzaware@gmail.com>
2024-06-08 11:39:01 +0100

    Got tests passing

--- Tests/PSSA.Tests.ps1
+++ Tests/PSSA.Tests.ps1
@@ -1,33 +1,58 @@
-BeforeAll {
+BeforeDiscovery { # <- this will run during Discovery
     $TestLocation = $(Split-Path $PSCommandPath -Parent)
-    # $TestLocation = Get-Location
-    $analysis = Invoke-ScriptAnalyzer -Path $TestLocation -Recurse -ExcludeRule PSAvoidUsingWriteHost, PSAvoidUsingPlainTextForPassword, PSAvoidUsingConvertToSecureStringWithPlainText
+    $analysis = Invoke-ScriptAnalyzer -Path $TestLocation # -Recurse -ExcludeRule PSAvoidUsingWriteHost, PSAvoidUsingPlainTextForPassword, PSAvoidUsingConvertToSecureStringWithPlainText
+    $warnings = $analysis.ForEach({ if ($_.Severity -match 'Warning') { $_ } })
+    $infomation = $analysis.ForEach({ if ($_.Severity -match 'Information') { $_ } })
+    $errors = $analysis.ForEach({ if ($_.Severity -match 'Error') { $_ } })
+    $ParseErrors = $analysis.ForEach({ if ($_.Severity -match 'ParseError') { $_ } })
     $scriptAnalyzerRules = Get-ScriptAnalyzerRule
-    write-host 'script root location is:' $TestLocation
-    Get-Module PesterDemoFunctions | Remove-Module
-    Import-Module $(Join-Path $(Split-Path $TestLocation -Parent)  "Modules/PesterDemoFunctions")
 }
+
+BeforeAll {
+    Get-Module PesterDemoFunctions | Remove-Module
+    Import-Module $(Join-Path $(Split-Path $(Split-Path $PSCommandPath -Parent) -Parent)  "Modules/PesterDemoFunctions")
+}
+    
 Context 'PSSA Standard Rules' {
     Describe 'Testing against PSSA rules' {
-        It 'TestLocation Should Not be null or empty' {
-            $TestLocation | Should -Not -BeNullOrEmpty                        
+        It 'TestLocation Should be location' -TestCases @{result = $TestLocation } {
+            $expected = $(Split-Path $PSCommandPath -Parent)
+            $result | Should -Be $expected                   
         }
-        It 'Analysys should not be null or empty' {
-            $analysis | Should -Not -BeNullOrEmpty                        
+        It 'Analysys  count should greater than 0'-TestCases @{result = $analysis } {
+            $result.Count | Should -BeGreaterThan 0                       
         }
-        It 'scriptAnalyzerRules should not be null or empty' {
-            $scriptAnalyzerRules | Should -Not -BeNullOrEmpty                        
+        It 'scriptAnalyzerRules count should be 70' -TestCases @{result = $scriptAnalyzerRules } {
+            $result.Count | Should -BeExactly  70                        
         }
-        forEach ($rule in $scriptAnalyzerRules){
-            
+        It 'Something' -ForEach $analysis {
+
         }
     }
+}
+Context 'Examples from Pester Docs' {
     Describe "Get-Emoji" {
         It "Returns <expected> (<name>)" -ForEach @(
             @{ Name = "cactus"; Expected = '🌵' }
             @{ Name = "giraffe"; Expected = '🦒' }
+            @{ Name = "apple"; Expected = '🍎' }
+            @{ Name = "pencil"; Expected = '✏️' }
+            @{ Name = "penguin"; Expected = '🐧' }
+            @{ Name = "smiling face with smiling eyes"; Expected = '😊' } 
         ) {
             Get-Emoji -Name $name | Should -Be $expected
+        }
+    }
+    Describe "Get-Emoji <name>" -ForEach @(
+        @{ Name = "cactus"; Symbol = '🌵'; Kind = 'Plant' }
+        @{ Name = "giraffe"; Symbol = '🦒'; Kind = 'Animal' }
+    ) {
+        It "Returns <symbol>" {
+            Get-Emoji -Name $name | Should -Be $symbol
+        }
+
+        It "Has kind <kind>" {
+            Get-Emoji -Name $name | Get-EmojiKind | Should -Be $kind
         }
     }
 }
--- Tests/scratch.ps1
+++ Tests/scratch.ps1
@@ -1,2 +1,13 @@
-$root = Split-Path -Parent $(split-path -Parent $MyInvocation.MyCommand.Path)
-Import-Module  $(Join-Path $root "modules" MyModule) -Verbose -Force
+$TestLocation = $(Split-Path $PSCommandPath -Parent)
+# $TestLocation = Get-Location
+$analysis = Invoke-ScriptAnalyzer -Path $TestLocation # -Recurse -ExcludeRule PSAvoidUsingWriteHost, PSAvoidUsingPlainTextForPassword, PSAvoidUsingConvertToSecureStringWithPlainText
+$analysis
+
+$warnings = $analysis.ForEach({ if ($_.Severity -match 'Warning') { $_ } })
+$infomation = $analysis.ForEach({ if ($_.Severity -match 'Information') { $_ } })
+$errors = $analysis.ForEach({ if ($_.Severity -match 'Error') { $_ } })
+$ParseErrors = $analysis.ForEach({ if ($_.Severity -match 'ParseError') { $_ } })
+
+$analysis.ForEach(
+    { RuleName = $_.RuleName ; Expected = $false }
+)
