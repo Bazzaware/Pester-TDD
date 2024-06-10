9f301b44303ec3bf5ca0991e3bec7e69e022b3f1
Bazza <bazzaware@gmail.com>
2024-06-10 07:21:19 +0100

    moving computers

--- Tests/PSSA.Tests.ps1
+++ Tests/PSSA.Tests.ps1
@@ -1,11 +1,8 @@
 BeforeDiscovery { # <- this will run during Discovery
     $TestLocation = $(Split-Path $PSCommandPath -Parent)
     $analysis = Invoke-ScriptAnalyzer -Path $TestLocation -Recurse #-ExcludeRule PSAvoidUsingWriteHost, PSAvoidUsingPlainTextForPassword, PSAvoidUsingConvertToSecureStringWithPlainText
-    $warnings = $analysis.ForEach({ if ($_.Severity -match 'Warning') { $_ } })
-    $infomation = $analysis.ForEach({ if ($_.Severity -match 'Information') { $_ } })
-    $errors = $analysis.ForEach({ if ($_.Severity -match 'Error') { $_ } })
-    $ParseErrors = $analysis.ForEach({ if ($_.Severity -match 'ParseError') { $_ } })
     $scriptAnalyzerRules = Get-ScriptAnalyzerRule
+    $ScriptAnalyzerRuleNames = $(Get-ScriptAnalyzerRule).RuleName
     $rules = @()
     foreach ($rule in $scriptAnalyzerRules) {
         $rules += @{
@@ -31,60 +28,15 @@ Context 'PSSA Standard Rules' {
             $result.Count | Should -BeExactly  70                        
         }
     }
-    Describe "Should pass <RuleName>" -ForEach $rules {
+
+
+    
+    Describe "Should pass <RuleName>" -ForEach $ScriptAnalyzerRuleNames {
         BeforeAll {
             $rule = $_
         }
         It "Should" -TestCases @{result = $analysis } {
             $result.RuleName | Should -Not -Match $rule.RuleName 
-        }
-    }
-}
-Context 'Examples from Pester Docs' {
-    Describe "Get-Emoji" {
-        It "Returns <expected> (<name>)" -ForEach @(
-            @{ Name = "cactus"; Expected = '🌵' }
-            @{ Name = "giraffe"; Expected = '🦒' }
-            @{ Name = "apple"; Expected = '🍎' }
-            @{ Name = "pencil"; Expected = '✏️' }
-            @{ Name = "penguin"; Expected = '🐧' }
-            @{ Name = "smiling face with smiling eyes"; Expected = '😊' } 
-        ) {
-            Get-Emoji -Name $name | Should -Be $expected
-        }
-    }
-    Describe "Get-Emoji <name>" -ForEach @(
-        @{ Name = "cactus"; Symbol = '🌵'; Kind = 'Plant' }
-        @{ Name = "giraffe"; Symbol = '🦒'; Kind = 'Animal' }
-    ) {
-        It "Returns <symbol>" {
-            Get-Emoji -Name $name | Should -Be $symbol
-        }
-
-        It "Has kind <kind>" {
-            Get-Emoji -Name $name | Get-EmojiKind | Should -Be $kind
-        }
-    }
-
-    Describe "Get-FruitBasket" {
-        It "Contains <_>" -ForEach '🍎', '🍌', '🥝', '🥑', '🍇', '🍐' {
-            Get-FruitBasket | Should -Contain $_
-        }
-    }
-
-    Describe "Get-FruitBasket" {
-        Context "Fruit <_>" -ForEach '🍎', '🍌', '🥝', '🥑', '🍇', '🍐' {
-            It "Contains <_> by default" {
-                Get-FruitBasket | Should -Contain $_
-            }
-
-            It "Can remove <_> from the basket" {
-                Remove-FruitBasket -Item $_
-                Get-FruitBasket | Should -Not -Contain $_
-            }
-        }
-        AfterAll {
-            Reset-FruitBasket
         }
     }
 }
--- Tests/scratch.ps1
+++ Tests/scratch.ps1
@@ -1,13 +1,53 @@
 $TestLocation = $(Split-Path $PSCommandPath -Parent)
 # $TestLocation = Get-Location
-$analysis = Invoke-ScriptAnalyzer -Path $TestLocation # -Recurse -ExcludeRule PSAvoidUsingWriteHost, PSAvoidUsingPlainTextForPassword, PSAvoidUsingConvertToSecureStringWithPlainText
-$analysis
+$analysis = Invoke-ScriptAnalyzer -Path . -Recurse # -ExcludeRule PSAvoidUsingWriteHost, PSAvoidUsingPlainTextForPassword, PSAvoidUsingConvertToSecureStringWithPlainText
+$scriptAnalyzerRules = Get-ScriptAnalyzerRule
 
 $warnings = $analysis.ForEach({ if ($_.Severity -match 'Warning') { $_ } })
 $infomation = $analysis.ForEach({ if ($_.Severity -match 'Information') { $_ } })
 $errors = $analysis.ForEach({ if ($_.Severity -match 'Error') { $_ } })
 $ParseErrors = $analysis.ForEach({ if ($_.Severity -match 'ParseError') { $_ } })
 
-$analysis.ForEach(
-    { RuleName = $_.RuleName ; Expected = $false }
+$ScriptAnalyzerRuleNames = $(Get-ScriptAnalyzerRule).RuleName
+$analysis | ft RuleName, Severity, ScriptName, Line, Message -AutoSize
+$files = Get-ChildItem "../*.ps1" -Recurse
+
+$results = $analysis.ForEach(
+    { @(
+            @{
+                RuleName   = $_.RuleName ;
+                ScriptName = $_.ScriptName;
+                Line       = $_.Line ;
+                Severity   = $_.Severity
+                Message    = $_.Message;
+            }
+        )
+    }
 )
+$results
+$rules = @()
+foreach ($rule in $scriptAnalyzerRules) {
+    $rules += @{
+        RuleName = $_.RuleName;
+    }
+}
+
+Describe -Tags 'PSSA' -Name 'Testing against PSScriptAnalyzer rules' {
+    Context 'PSSA Standard Rules' {
+        $ScriptAnalyzerSettings = Get-Content -Path "$PSScriptRoot\..\ScriptAnalyzerSettings.psd1" | Out-String | Invoke-Expression
+        $AnalyzerIssues = Invoke-ScriptAnalyzer -Path "$PSScriptRoot\..\MyScript.ps1" -Settings "$PSScriptRoot\..\ScriptAnalyzerSettings.psd1"
+        $ScriptAnalyzerRuleNames = Get-ScriptAnalyzerRule | Select-Object -ExpandProperty RuleName
+        forEach ($Rule in $ScriptAnalyzerRuleNames) {
+            $Skip = @{Skip = $False }
+            if ($ScriptAnalyzerSettings.ExcludeRules -notcontains $Rule) {
+                # We still want it in the tests, but since it doesn't actually get tested we will skip
+                $Skip = @{Skip = $True }
+            }
+
+            It "Should pass $Rule" @Skip {
+                $Failures = $AnalyzerIssues | Where-Object -Property RuleName -EQ -Value $rule
+                ($Failures | Measure-Object).Count | Should Be 0
+            }
+        }
+    }
+}
